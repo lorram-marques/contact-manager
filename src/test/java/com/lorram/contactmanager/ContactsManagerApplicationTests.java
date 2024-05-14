5cfc827aa7d808649da1caefff19f49385f3cf6c
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2024-05-14 08:59:27 -0300

    Project created

---
+++ src/test/java/com/lorram/contactmanager/ContactsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorram.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
